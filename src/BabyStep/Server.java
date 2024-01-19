6013758e90b20b55411542902388204fc67e3ee6
Romain Miras <romain.miras@etu.univ-grenoble-alpes.fr>
2024-01-19 10:36:37 +0100

    Baby Server

--- src/BabyStep/Server.java
+++ src/BabyStep/Server.java
@@ -1,6 +1,12 @@
 package BabyStep;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
@@ -8,12 +14,30 @@ public class Server {
 	public static void main(String[] args) throws InvalidPropertiesFormatException, IOException {
 		Properties prop = new Properties();
 		prop.loadFromXML(Server.class.getClassLoader().getResourceAsStream("info.xml"));
-		String ipServer = prop.getProperty("ipServer");
 		int portServer = Integer.parseInt(prop.getProperty("portServer"));
-		
+
 		System.out.println("Server is running...");
-		System.out.println("IP: " + ipServer);
-		System.out.println("Port: " + portServer);
+
+		ServerSocket listenSoc = new ServerSocket(portServer);
+
+		Socket soc = listenSoc.accept();
+		DataInputStream dis = null;
+		DataOutputStream dos = null;
+		try {
+			InputStream is = soc.getInputStream();
+			dis = new DataInputStream(is);
+			String clientName = dis.readUTF();
+			
+			OutputStream os = soc.getOutputStream();
+			dos = new DataOutputStream(os);
+			dos.writeUTF("Hello " + clientName);
+		} catch ( Exception e ) {
+
+		} finally {
+
+			if (dis != null) try { dis.close();} catch (Exception e) {}
+
+		}
 	}
 
 }
