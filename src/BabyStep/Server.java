383f3d0e536f18027a5258e8e9a62ec6c1066c2a
Romain Miras <romain.miras@etu.univ-grenoble-alpes.fr>
2024-01-19 10:24:16 +0100

    XML

--- src/BabyStep/Server.java
+++ src/BabyStep/Server.java
@@ -1,5 +1,19 @@
 package BabyStep;
 
+import java.io.IOException;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Properties;
+
 public class Server {
+	public static void main(String[] args) throws InvalidPropertiesFormatException, IOException {
+		Properties prop = new Properties();
+		prop.loadFromXML(Server.class.getClassLoader().getResourceAsStream("info.xml"));
+		String ipServer = prop.getProperty("ipServer");
+		int portServer = Integer.parseInt(prop.getProperty("portServer"));
+		
+		System.out.println("Server is running...");
+		System.out.println("IP: " + ipServer);
+		System.out.println("Port: " + portServer);
+	}
 
 }
